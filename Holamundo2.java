235b6f402b81870d030d7906b287658a117e2592
josefbustam <josefbustam@gmail.com>
2015-07-24 21:40:28 -0400

    Hola mundo v2

---
+++ Holamundo2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author JsoE_
+ */
+public class Holamundo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+      System.out.println("hola mundo");
+      System.out.println("Esto es la segunda prueba de control de versiones");// TODO code application logic here
+    }
+    
+}
