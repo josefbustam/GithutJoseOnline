347afd805d84fe18ab12c6733d0920192e1d6fb4
josefbustam <josefbustam@gmail.com>
2015-07-24 22:13:21 -0400

    pruebaGITHUBv2(modificable)
    prueba numero 2

---
+++ PruebaGITHUBv2/src/pruebagithubv2/PruebaGITHUBv2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagithubv2;
+
+/**
+ *
+ * @author JsoE_
+ */
+public class PruebaGITHUBv2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       System.out.println("hola mundo para GITHUB");
+        int area_triangulo;
+        int base = 6;
+        int altura = 3;
+      
+      
+        area_triangulo = (base*altura)/2;
+        System.out.println("area de un triangulo = "+area_triangulo);
+    }
+    }
+    
+
