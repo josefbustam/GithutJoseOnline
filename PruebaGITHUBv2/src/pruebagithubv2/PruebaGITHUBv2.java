b1c5cb412b92d7dc22033d38f5e6eed659e405e3
josefbustam <josefbustam@gmail.com>
2015-07-24 22:56:59 -0400

    PruebaGITHUBv2 (modificablev1.4)
    holamundo+area_triangulo+scanner+BufferedReader

--- PruebaGITHUBv2/src/pruebagithubv2/PruebaGITHUBv2.java
+++ PruebaGITHUBv2/src/pruebagithubv2/PruebaGITHUBv2.java
@@ -5,6 +5,9 @@
  */
 package pruebagithubv2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
@@ -16,7 +19,7 @@ public class PruebaGITHUBv2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException 
     {
 //********************Inicio hola mundo*************************
        System.out.println("hola mundo para GITHUB version 2");
@@ -33,27 +36,50 @@ public class PruebaGITHUBv2 {
         
         
 //********************INICIO USO DE SCANNER*****************
+        System.out.println("");
+        System.out.println("USO DE SCANNER");	
         //Creo una variable tipo String con nombre: "nombre" 
         String nombre; 
         // Creo mi Scanner con nombre: "T" 
         Scanner T = new Scanner (System.in); 
 
         //Inicio mi for con una variable "i" comenzada en 0 ( cero), hasta que i sea mener que 4 e "i" valla aumnetado en 1 
+        System.out.println("=============Bienvenido=============" ); 
         for (int i = 0 ; i<4 ; i++)
         { 
             //Comienza cintenido del for 
+            
             System.out.print("Ingrese nombre N° "+(i+1)+": " ); 
             nombre = T.nextLine(); 
 
-            System.out.println("=============Bienvenido=============" ); 
+            
             System.out.println("Bienvenido "+ nombre); 
-            System.out.println("====================================" ); 
+            
             //finaliza contenido del for 
         } 
         System.out.println("Todos los nombres fueron ingresados exitosamente." ); 
 //********************FIN USO DE SCANNER*************************
-} 
+        
+        
+//********************INICIO USO DE BUFFEREDREADER*************************      
+        
+        BufferedReader br;
+        br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("");
+        System.out.println("USO DE BufferedReader");	
+        System.out.println("=============Bienvenido=============" ); 
+            System.out.print("Introduce el primer número:");			
+            int numero1 = Integer.parseInt(br.readLine());			
+            System.out.print("Introduce el segundo número:");
+            int numero2 = Integer.parseInt(br.readLine()); 
+            int resultado = numero1*numero2;
+            System.out.println("");
+            System.out.println("La multiplicación es " + numero1 + " x " + numero2 + " = " + resultado);
+        
+//********************FIN USO DE BUFFEREDREADER*************************        
+    }
 }
+
     
     
 
